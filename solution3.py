5fd4a9bc378856a6d9b1e362f8398da8e6d4ac5d
samarthahuja610 <54635554+samarthahuja610@users.noreply.github.com>
2019-11-28 22:13:51 +0530

    Update solution3.py

--- solution3.py
+++ solution3.py
@@ -1,5 +1,8 @@
-while(1):
-    try:
-        n = int(input())
-    except:
-        print('error Occured')
+try:
+    n = int(input())
+except ValueError:
+    raise ValueError('value error occured')
+except KeyboardInterrupt:
+    raise KeyboardInterrupt('keyboard intrupt error occured')
+except EOFError:
+    raise EOFError('end of file error')
